e1f4657ba7288ace36301c4a5fadd91b4acacea3
AytajMehraliyeva <tu7lzxxdc@code.edu.az>
2024-01-10 16:23:17 +0400

    back

--- back/server.js
+++ back/server.js
@@ -1,19 +1,21 @@
-const express = require("express")
 const cors = require("cors")
+const express = require("express")
+
 require('dotenv').config()
 const mongoose = require("mongoose")
-const DataRouter= require('./routing/Data.routes')
+const ProductRouter = require("./routing/Data.routes")
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 2023
+
 app.use(cors())
 app.use(express.json())
-app.use("/datas", DataRouter)
+app.use("/datas", ProductRouter)
 
 
-mongoose.connect(`mongodb+srv://tu7lzxxdc:aytac123@cluster0.fvv4h2i.mongodb.net/`).then(res => {
-    console.log("Connect to db")
+mongoose.connect("mongodb+srv://tu7lzxxdc:aytac123@cluster0.fvv4h2i.mongodb.net/").then(res => {
+    console.log("Connected to db")
 })
 
 app.listen(PORT, () => {
-    console.log("App running on 5000")
+    console.log("App running on 2023")
 })
